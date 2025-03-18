09c84bfe0b7e0cc98cef03835c755513a8821050
barbara 💌 <110749315+barbaraeguche@users.noreply.github.com>
2025-03-18 14:06:00 -0400

    feat: create find-the-index-of-the-first-occurrence-in-a-string.ts

---
+++ easy/find-the-index-of-the-first-occurrence-in-a-string.ts
@@ -0,0 +1,7 @@
+# name: 28. find the index of the first occurrence in a string.ts
+# link: https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string
+
+# solution #
+function strStr(haystack: string, needle: string): number {
+  return haystack.indexOf(needle)
+};
