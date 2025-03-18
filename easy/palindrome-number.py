1873ec08f4c8c674c8f7deae8107afaaa142c5e1
barbara 💌 <110749315+barbaraeguche@users.noreply.github.com>
2025-03-18 12:33:55 -0400

    docs: update palindrome-number.py

--- easy/palindrome-number.py
+++ easy/palindrome-number.py
@@ -1,4 +1,17 @@
 # name: 9. palindrome number
-# link: https://leetcode.com/problems/palindrome-number
+# link: [https://leetcode.com/problems/palindrome-number](https://leetcode.com/problems/palindrome-number)
 
 # solution #
+class Solution:
+  def isPalindrome(self, x: int) -> bool:
+    int_str = str(x)
+    a, b = 0, len(int_str) - 1
+
+    while a < b:
+      if int_str[a] != int_str[b]:
+        return False
+      else:
+        a += 1
+        b -= 1
+            
+    return True
