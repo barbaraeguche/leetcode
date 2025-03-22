67135d0ae67d97d003bdba07eb0b9356956a991c
barbara 💌 <barbaraeguche@icloud.com>
2025-03-22 08:22:54 -0400

    feat: solved 70 as of now :)

---
+++ easy/merge-strings-alternately.py
@@ -0,0 +1,15 @@
+# name: 1768. merge strings alternately
+# link: https://leetcode.com/problems/merge-strings-alternately
+
+# solution #
+class Solution:
+	def mergeAlternately(self, word1: str, word2: str) -> str:
+		min_word = min(word1, word2, key=len)
+		max_word = word1 if min_word != word1 else word2
+		
+		merged = [""] * len(min_word)
+		
+		for i in range(len(min_word)):
+			merged[i] = word1[i] + word2[i]
+		
+		return "".join(merged) + max_word[len(min_word):]
