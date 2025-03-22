d2a615351538745b93229ac7418d829770cf1c14
barbara 💌 <barbaraeguche@icloud.com>
2025-03-22 12:54:49 -0400

    Revert "refactor: move tree questions into three folder"
    This reverts commit cfb9ad82e16f2d175df9c1aeffa420659dd6c3b5.

--- easy/database/replace-employee-id-with-the-unique-identifier.sql
+++
@@ -1,7 +0,0 @@
--- name: 1378. replace employee id with the unique identifier
--- link: https://leetcode.com/problems/replace-employee-id-with-the-unique-identifier
-
--- solution --
-select unique_id, name
-from Employees e
-	left join EmployeeUNI eu on e.id = eu.id
