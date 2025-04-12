2107aede84ce2c7be5621faa4839fd1166fde305
barbara 💌 <barbaraeguche@icloud.com>
2025-04-12 13:38:52 -0400

    feat: solved ... as of now :)

--- medium/reverse-integer.py
+++ medium/reverse-integer.py
@@ -5,7 +5,7 @@
 class Solution:
 	def reverse(self, x: int) -> int:
 		rev_x = str(-1 * x if x < 0 else x)[::-1]
-		min_int = -2147483648; max_int = 2147483647
+		min_int, max_int = -2147483648, 2147483647
 		
 		if not min_int <= int(rev_x) <= max_int:
 			return 0
