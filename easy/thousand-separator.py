3ec3750a119a73481696a7ca0a4dd068f6ff1958
barbara 💌 <barbaraeguche@icloud.com>
2025-04-27 15:30:20 -0400

    refactor

--- easy/thousand-separator.py
+++ easy/thousand-separator.py
@@ -20,3 +20,17 @@ class Solution:
 		
 		return ".".join(splitted)
 	
+	def thousandSeparator(self, n: int) -> str:
+		string = str(n)
+		
+		if len(string) <= 3:
+			return string
+		
+		rem = len(string) % 3
+		array = [string[i:i+3] for i in range(rem, len(string), 3)]
+		
+		if rem != 0:
+			array = [string[:rem]] + array
+		
+		return ".".join(array)
+	
