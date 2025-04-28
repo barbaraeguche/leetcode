aeccc04f6ab6884c9a8b3c8b29fc4dd1526d0d72
barbara 💌 <barbaraeguche@icloud.com>
2025-04-28 12:51:00 -0400

    refactor

--- easy/rings-and-rods.py
+++ easy/rings-and-rods.py
@@ -13,3 +13,15 @@ class Solution:
 		total = [k for k,v in rr_dict.items() if len(set(v)) == 3]
 		return len(total)
 	
+	def countPoints(self, rings: str) -> int:
+		count = 0
+		
+		for i in range(10):
+			string = str(i)
+			b, g, r = f"B{string}", f"G{string}", f"R{string}"
+			
+			if b in rings and g in rings and r in rings:
+				count += 1
+		
+		return count
+	
