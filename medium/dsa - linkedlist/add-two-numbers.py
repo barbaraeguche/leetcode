da78979e570f3f6c9f585063001e822d1ccbac57
barbaraeguche <barbaraeguche@icloud.com>
2025-05-13 19:52:52 -0400

    feat: solved 326 as of now :)

--- medium/dsa - linkedlist/add-two-numbers.py
+++ medium/dsa - linkedlist/add-two-numbers.py
@@ -25,3 +25,30 @@ class Solution:
 		
 		return int(list_str[::-1])
 	
+	
+	##################### ---------------------- #####################
+	
+	def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+		return self.addNums(l1, l2, 0)
+	
+	@staticmethod
+	def addNums(l1: Optional[ListNode], l2: Optional[ListNode], carry: int) -> Optional[ListNode]:
+		# if no node and carry-over is empty
+		if not l1 and not l2 and carry == 0:
+			return None
+		
+		# get the values
+		v1 = l1.val if l1 else 0
+		v2 = l2.val if l2 else 0
+		
+		# perform arithmetic
+		rem, quot = divmod(v1 + v2 + carry, 10)
+		
+		# calculate the next value
+		node = Solution.addNums(
+			l1.next if l1 else None,
+			l2.next if l2 else None,
+			rem
+		)
+		
+		return ListNode(quot, node)
---
+++ medium/dsa - trees/kth-smallest-element-in-a-bst.py
@@ -0,0 +1,17 @@
+# name: 230. kth smallest element in a bst
+# link: https://leetcode.com/problems/kth-smallest-element-in-a-bst
+
+# solution #
+class Solution:
+	def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+		inorder = []
+		
+		def traversal(root: Optional[TreeNode]) -> None:
+			if root:
+				traversal(root.left)
+				inorder.append(root.val)
+				traversal(root.right)
+		
+		traversal(root)
+		return inorder[k - 1]
+	
