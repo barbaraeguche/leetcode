1c2da936230d8d245c509319166bf0937a72ce85
barbaraeguche <barbaraeguche@icloud.com>
2025-05-25 21:27:38 -0400

    feat: solved 340 as of now :)

--- medium/dsa - linkedlist/add-two-numbers.py
+++ medium/dsa - linkedlist/add-two-numbers.py
@@ -4,31 +4,6 @@
 # solution #
 class Solution:
 	def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-		added = str(self.list_to_int(l1) + self.list_to_int(l2))[::-1]
-		
-		head = ListNode(int(added[0]))
-		curr = head
-		
-		for num in added[1:]:
-			curr.next = ListNode(int(num))
-			curr = curr.next
-		
-		return head
-	
-	@staticmethod
-	def list_to_int(list: Optional[ListNode]):
-		list_str = ""
-		
-		while list:
-			list_str += str(list.val)
-			list = list.next
-		
-		return int(list_str[::-1])
-	
-	
-	##################### ---------------------- #####################
-	
-	def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
 		return self.addNums(l1, l2, 0)
 	
 	@staticmethod
