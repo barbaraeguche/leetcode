17d737c9655b2289d2c433434242bb04410cf467
barbaraeguche <barbaraeguche@icloud.com>
2025-05-27 19:48:06 -0400

    feat: solved 351 as of now :)

--- easy/dsa - two pointers/valid-palindrome.py
+++ easy/dsa - two pointers/valid-palindrome.py
@@ -4,5 +4,17 @@
 # solution #
 class Solution:
   def isPalindrome(self, s: str) -> bool:
-    alpha_num = ''.join(filter(str.isalnum, s)).lower()
-    return alpha_num == alpha_num[::-1]
+    l, r = 0, len(s) - 1
+    
+    while l < r:
+      while l < r and not s[l].isalnum():
+        l += 1
+      while r > l and not s[r].isalnum():
+        r -= 1
+      
+      if not s[l].lower() == s[r].lower():
+        return False
+      
+      l, r = l + 1, r - 1
+    
+    return True
